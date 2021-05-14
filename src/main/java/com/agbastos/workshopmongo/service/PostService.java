3ea7aae86f8505e9963cb493ea1d14c2f00af3ce
André Gustavo Bastos <agbastos@hotmail.com>
2021-05-14 04:47:41 -0300

    Consulta simples com query methods

--- src/main/java/com/agbastos/workshopmongo/service/PostService.java
+++ src/main/java/com/agbastos/workshopmongo/service/PostService.java
@@ -1,5 +1,6 @@
 package com.agbastos.workshopmongo.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,4 +21,7 @@ public class PostService {
 		return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
+	public List<Post> findByTitleContaining(String text) {
+		return postRepository.findByTitleContainingIgnoreCase(text);
+	}
 }
